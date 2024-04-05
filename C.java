4ce49fed633e3fa83257a02c8d5f7f3e77ed55c6
neonik <lis.gleb2012@gmail.com>
2024-04-05 10:34:35 +0300

    r4

--- C.java
+++ C.java
@@ -2,7 +2,7 @@ public class C extends E {
 
     private int g = 1;
 
-    private double h = 100.50;
+    private double h = 100.500;
 
     public java.util.Random mm() {
         return new java.util.Random();
@@ -10,5 +10,9 @@ public class C extends E {
 
     public double ee() {
         return java.lang.Math.PI;
+    }
+
+    public Object pp() {
+        return this;
     }
 }
